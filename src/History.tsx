6f103ff49731277a1dfbac123714f5daf73d87c6
waterfall <78034265+wtrfl@users.noreply.github.com>
2026-07-06 13:24:25 +0200

    feat: make earliest and latest update when a shift is deleted

--- src/History.tsx
+++ src/History.tsx
@@ -10,10 +10,52 @@ export default function History() {
 
     useEffect(() => {
         localStorage.setItem("data", JSON.stringify(data));
+        console.log("new earliest:", data.earliest)
+        console.log("new latest:", data.latest)
     }, [data])
 
     const handleDelete = (id: string) => {
-        setData(d => ({ ...d, history: d.history.filter(i => i.id !== id) }))
+        const deletedItem = data.history.find(shift => shift.id == id);
+        if (deletedItem === undefined) return;
+
+        setData(d => ({ ...d, history: d.history.filter(i => i.id !== id) }));
+
+        if (!data.earliest || !data.latest) return;
+
+        if (data.history.length == 1) {
+            setData(d => ({ ...d, earliest: null, latest: null }));
+            return;
+        }
+
+        const deletedDate = new Date(deletedItem.date);
+        const earliestDate = new Date(data.earliest);
+        const latestDate = new Date(data.latest);
+
+        if (deletedDate <= earliestDate) {
+            let newEarliest = new Date("2100-01-01");
+
+            for (let i = 0; i < data.history.length; i++) {
+                let current = new Date(data.history[i].date)
+
+                if (current < newEarliest && data.history[i].id !== id) {
+                    newEarliest = current;
+                }
+            }
+            setData(d => ({ ...d, earliest: newEarliest }))
+        }
+
+        if (deletedDate >= latestDate) {
+            let newLatest = new Date("2000-01-01");
+
+            for (let i = 1; i < data.history.length; i++) {
+                let current = new Date(data.history[i].date) 
+
+                if (current > newLatest && data.history[i].id !== id) {
+                    newLatest = current;
+                }
+            }
+            setData(d => ({ ...d, latest: newLatest }))
+        }
     }
 
     const periods = () => {
@@ -30,7 +72,7 @@ export default function History() {
             currentDate = new Date(currentDate.getTime() + TWO_WEEKS_MS);
         }
         result.push(new Date(currentDate.getTime()));
-        
+
         return result;
     }
 
